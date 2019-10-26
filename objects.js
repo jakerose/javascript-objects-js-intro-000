14e2181e7be84d62723b0167e07d6a40113be3c6
Jake Rose <jake1020@mac.com>
2019-10-26 20:49:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-delete playlist.NSYNC
+delete playlist.artistName
 artistName;
 }
