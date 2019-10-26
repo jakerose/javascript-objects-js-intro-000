738d4b7ab9499788215d915b8df919278cd1bb49
Jake Rose <jake1020@mac.com>
2019-10-26 20:52:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
 delete playlist.NSYNC
-delete playlist.PHILOCHS
+delete playlist.Slowdive
 delete playlist.MYBLOODYVALENTINE
 artistName;
 }
