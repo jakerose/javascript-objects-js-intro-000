a41a2b079ecd92b0efc1d34b7972c343864e47e9
Jake Rose <jake1020@mac.com>
2019-10-26 20:44:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {NSYNC: "Bye bye bye"};
+var playlist = {NSYNC: "Bye bye bye", My Bloody Valentine: "Sometimes"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
